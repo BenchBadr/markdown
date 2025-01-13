83d5cdca1906bfccf3c593b1e5115397bca16323
Arlequiin <78297845+Arlequiin@users.noreply.github.com>
2025-01-13 01:12:44 +0100

    ready for fun

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import Editor from "./components/editor";
 function App() {
   return (
     <div className="landing">
-      <h1>Merjitur</h1>
       <Editor/>
     </div>
   );
---
+++ client/src/util/text/render.js
@@ -0,0 +1,5 @@
+const tokenToHtml = (token) => {
+
+}
+
+export default tokenToHtml;
